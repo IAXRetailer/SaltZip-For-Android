04a96760157b4895c99b179de799cd44274db75a
H2Sxxa <retailer673@163.com>
2022-06-10 19:25:06 +0800

    debug

--- sfa/main.py
+++ sfa/main.py
@@ -8,25 +8,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.lang.builder import Builder
-from library.Core.Hash import des
-from library.Core.Zip import zip as Izip
-from library.Core.Bitlayer import BitString
-from os.path import dirname,exists
 from kivy.core.text import LabelBase
-from android.permissions import request_permissions, Permission
-request_permissions([
-    Permission.WRITE_EXTERNAL_STORAGE,
-    Permission.READ_EXTERNAL_STORAGE
-])
 LabelBase.register(name='Han_Font',fn_regular='./fonts/b.ttf')
-
 Builder.load_file("main.kv")
-
-
-class FinishPopupBox(BoxLayout):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-
 class WarningPopup(Popup):
     def __init__(self, parent_inst, *args,  **kwargs):
         super(WarningPopup, self).__init__(*args, **kwargs)
@@ -37,50 +21,13 @@ class ChoseFile(BoxLayout):
         self.orientation = "vertical"
         self.fichoo = FileChooserListView(font_name="Han_Font")
         self.popup = WarningPopup(self)
-        btn_delete = Button(text="选择文件", on_release=self.popup.open, size_hint_y=0.1,font_name="Han_Font")
+        btn_delete = Button(text="Chose file", on_release=self.popup.open, size_hint_y=0.1,font_name="Han_Font")
 
         self.add_widget(self.fichoo)
         self.add_widget(btn_delete)
 
     def selete(self, *args):
-        try:
-            hkpath=self.fichoo.selection[0]
-        except:
-            return
-        if ".zip" in hkpath:
-            try:
-                Izip.unzip(hkpath,None,dirname(hkpath))
-            except:
-                pass
-            return
-        if ".rar" in hkpath:
-            try:
-                Izip.unrarfile(hkpath,dirname(hkpath))
-            except:
-                pass
-            return
-        if ".hk" in hkpath:
-            con=open(hkpath,"r").read()
-            filepath=hkpath.replace(".hk",".sip")
-            if not exists(filepath):
-                filepath.replace(".sip",".zip")
-            con=con.replace("saltzip://","")
-            conlist=con.split("|")
-            chuck1,chuck2,chuck3=conlist[0],conlist[1],conlist[2]
-            chuck3=des.b64d(chuck3).split("/!")
-            chuck2=des.b64d(chuck2).split("/!")
-            password=des.decodeStringhash(chuck1,chuck2[0],chuck2[1])
-            Izip.unzip(filepath,password,dirname(filepath))
-        elif ".h2k" in hkpath:
-            con=open(hkpath,"rb").read()
-            lenstr=hkpath.replace(".h2k","").split(".")[-1]
-            res=BitString.readrbtostring(hkpath,int(lenstr)+64+10)
-            password=des.decodeStringhash(res[:32],res[32:40],res[40:48])
-            dictory=dirname(hkpath)
-            unfilepath=hkpath.replace(lenstr+".h2k","sip")
-            Izip.unzip(unfilepath,password,dictory)
-        else:
-            pass
+        pass
 class SaltZip(App):
     def build(self):
         return ChoseFile()
