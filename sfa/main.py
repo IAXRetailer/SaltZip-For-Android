19645f1c25457875579fcd805d5d10e05c74dc1a
H2Sxxa <retailer673@163.com>
2022-06-11 19:10:32 +0800

    beautifl

--- sfa/main.py
+++ sfa/main.py
@@ -1,13 +1,15 @@
-import os
+from kivy.config import Config
+from kivymd.font_definitions import theme_font_styles
+Config.set('kivy', 'default_font', [
+    'Han_Font',
+    'fonts/b.ttf'])
+
 import kivy
 kivy.require('2.1.0')
-
 from kivymd.app import MDApp
 from kivy import user_home_dir
 from kivymd.uix.filemanager import MDFileManager
-from kivy.uix.filechooser import FileChooserListView
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.popup import Popup
 from kivy.lang.builder import Builder
 from library.Core.Hash import des
@@ -39,19 +41,18 @@ class WarningPopup(Popup):
     def __init__(self, parent_inst, *args,  **kwargs):
         super(WarningPopup, self).__init__(*args, **kwargs)
         self.parent_inst = parent_inst
-class ChoseFile(BoxLayout):
+class ChoseFile(MDBoxLayout):
     def __init__(self, *args, **kwargs):
         super(ChoseFile, self).__init__(*args, **kwargs)
         self.orientation = "vertical"
         self.fichoo = MDFileManager(select_path=self.selete)
-        self.popup = WarningPopup(self)
+        #self.fichoo.lab
         #btn_delete = Button(text="选择文件", on_release=self.popup.open, size_hint_y=0.1,font_name="Han_Font")
-        self.fichoo.show(os.path.dirname(user_home_dir))
+        self.fichoo.show(dirname(user_home_dir))
         #self.add_widget(self.fichoo)
         #self.add_widget(btn_delete)
 
     def selete(self, *args):
-        print("a")
         try:
             hkpath=self.fichoo.selection[0]
         except:
@@ -105,6 +106,10 @@ class ChoseFile(BoxLayout):
 
 class SaltZip(MDApp):
     def build(self):
+        self.theme_cls.theme_style="Dark"
+        self.theme_cls.primary_palette = "Orange"
+        theme_font_styles.append("Han_Font")
+        self.theme_cls.font_styles["Han_Font"]=["Han_Font",16,False,0.15]
         return ChoseFile()
 
 if __name__ == '__main__':
